8466cf0c1f14a6931ccd7f0140a77b5f9b7cdb6a
Andre Alcantara-Williams <aalcantarawillia@nyc.yearup.org>
2018-12-05 10:14:48 -0500

    added button

---
+++ main.js
@@ -0,0 +1,11 @@
+let button = document.getElementById('button');
+let modal = document.getElementById('modal');
+
+
+
+function black() {
+  modal.style.display ="block";
+}
+
+
+button.addEventListener('click', black);
